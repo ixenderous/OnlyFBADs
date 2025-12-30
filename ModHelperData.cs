4598d660864a0a629d75b2bf08b0db88f60a2b02
ixenderous <ixenderous@gmail.com>
2025-12-30 13:58:56 +1300

    Updated description and version

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,10 +3,10 @@ namespace OnlyFBADs;
 public static class ModHelperData
 {
     public const string WorksOnVersion = "52.0";
-    public const string Version = "1.1.1";
+    public const string Version = "1.1.2";
     public const string Name = "OnlyFBADs";
 
-    public const string Description = "While enabled, only FBADs will give pops in sandbox mode. Optional toggle for regular BADs to also give pops.";
+    public const string Description = "While enabled, only FBADs will give pops in sandbox mode. Optional toggle for regular BADs to also give pops. Optional toggle to just delete sub bads instead of preventing them from giving pops.";
 
     public const string RepoOwner = "ixenderous";
     public const string RepoName = "OnlyFBADs";
